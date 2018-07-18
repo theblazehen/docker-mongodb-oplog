1fca1c0dc0c2843056a7fd2be56d34019be577c9
Zadkiel Aslafy-Aharonian <aslafy_z@epitech.eu>
2018-07-18 15:36:45 +0200

    Set rs hostname to local hostname

--- init_repl.sh
+++ init_repl.sh
@@ -5,16 +5,18 @@ if [ -f /data/db/.repl_init_done ]; then
   exit 0
 fi
 
+host=${HOSTNAME:-$(hostname -f)}
+
 # start mongod for configuration
 mongod --replSet rs0 &
 # wait for mongod to be started
 while ! mongo --eval 'db.version()' > /dev/null 2>&1; do sleep 0.1; done
 
 # init replset with defaults
-mongo local --eval 'rs.initiate({
-   _id : "rs0",
-   members: [ { _id : 0, host : "mongo:27017" } ]
-})'
+mongo local --eval "rs.initiate({
+   _id: 'rs0',
+   members: [ { _id: 0, host: '$host:27017' } ]
+})"
 
 # send stop signal to mongod
 kill -2 %1
