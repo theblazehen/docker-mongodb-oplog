7ffce5be5f9792132e43c378d53cbe1dd4e5aa31
theblazehen <theblazehen@gmail.com>
2019-05-02 09:11:45 +0200

    Let it run in openshift

--- endpoint.sh
+++ endpoint.sh
@@ -2,8 +2,4 @@
 
 # Initialize replicaSet
 /init_repl.sh || exit $?
-
-# Make sure that database is owned by user mongodb
-[ "$(stat -c %U /data/db)" = mongodb ] || chown -R mongodb /data/db
-
-exec su -s /bin/sh -c "exec mongod --bind_ip_all --replSet rs0" mongodb
+exec mongod --bind_ip_all --replSet rs0
